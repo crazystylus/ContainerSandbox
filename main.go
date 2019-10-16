a1a1d0803ddc0f38aea72a850dc939887448dc0f
crazystylus <kartik.sharma522@gmail.com>
2019-10-16 19:23:01 +0530

    Adding UTS Namespace isolation

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 // docker 			run	<cmd>	<params>
@@ -13,6 +14,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("bad command")
 	}
@@ -21,6 +24,19 @@ func main() {
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
 
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+	cmd.Run()
+}
+
+func child() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+	syscall.Sethostname([]byte("container"))
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
